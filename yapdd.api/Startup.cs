44e2f35b4563a826a0c7ff12adc1755adeb1f72a
Nurzhan Aitbayev <nurzhait@gmail.com>
2019-12-30 01:39:38 +0600

    Removed unused and broken usings

--- yapdd.api/Startup.cs
+++ yapdd.api/Startup.cs
@@ -1,19 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using Swashbuckle.AspNetCore.Swagger;
 using yapdd.api.Services;
 using yapdd.api.Services.Contracts;
 
